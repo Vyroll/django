06696b20d9fafcdc0242f20c8d34f0eff8439597
Łukasz Nosal <lukasz94nosal@gmail.com>
2018-12-12 19:32:40 +0100

    logging, messages, basic template, simplified generic view, simplified logic in views

--- django/polls/views.py
+++ django/polls/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 from .models import Question, Choice
 
@@ -19,30 +24,46 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-    
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
 
+    choices = question.choice_set.all
+    question_id = question.id
+    question_text = question.question_text
+
+    return render(request, 'polls/detail.html', {
+        'question_id': question_id,
+        'choices':choices,
+        'question_text':question_text,
+        'title': "Detail",
+    })
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    context_object_name = 'question'
+        
+    def get_context_data(self, **kwargs):
+        context = super(ResultsView, self).get_context_data(**kwargs)
+        context['choices'] = context['question'].choice_set.all
+        context['question_text'] = context['question'].question_text
+        return context
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    choices = question.choice_set.all
+    question_id = question.id
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
+        messages.error(request, 'You didn\'t select a choice.')
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'question_id': question_id,
+            'choices':choices,
+            'title':'Vote result'
         })
     else:
         selected_choice.votes += 1
